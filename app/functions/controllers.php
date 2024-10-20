2b8e378619d61643d149d633d7726297efee85da
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-20 22:33:23 +0300

    process in controllers

--- app/functions/controllers.php
+++ app/functions/controllers.php
@@ -9,9 +9,32 @@ function home()
 //    } else {
 //        $size = $boysSize;
 //    }
-    render('home');
+    $errors = getErrors();
+    render('home' , [
+        'errors' => $errors
+    ]);
 //    render('home', [
 //        'names' => $names,
 //        'size' => $size,
 //    ]);
+}
+
+function process()
+{
+//    $names = getNames();
+    $data = [
+        'babyName' => htmlspecialchars(filter_input(INPUT_POST,'babyName')),
+        'gender' => htmlspecialchars(filter_input(INPUT_POST,'gender')),
+    ];
+    //validations
+    $errors = validateData($data, OPTIONS_ERROR);
+    if (empty($errors)) {
+//        writeData($data,'../data.json');
+    } else {
+        setErrors($errors);
+    }
+
+//    $names[$gender][] = $name;
+//    setNames($names);
+    redirect('/index.php');
 }
