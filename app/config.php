02a242e9c8222cc94c799f47d11512c30a0dee81
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-20 21:49:52 +0300

    add config

---
+++ app/config.php
@@ -0,0 +1,15 @@
+<?php
+const TEXT_SITE_NAME = 'Baby Names';
+const TEXT_FOOTER = 'Created Larysa Rozhniatovska, October 2024';
+
+
+//VIEWS all parameters of views===========================================================
+const VIEWS_TEMPLATE_MAIN = 'template_main';
+const VIEWS_DIR = 'app' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR;
+const VIEWS_TEMPLATES_DIR = VIEWS_DIR . 'templates' . DIRECTORY_SEPARATOR;
+const VIEWS_COMMON_DIR = VIEWS_DIR . 'common' . DIRECTORY_SEPARATOR;
+const VIEWS_PAGES_DIR = VIEWS_DIR . 'pages' . DIRECTORY_SEPARATOR;
+
+//FUNCTIONS all parameters================================================================
+const FUNCTIONS_DIR = APP_DIR . DIRECTORY_SEPARATOR . 'functions' . DIRECTORY_SEPARATOR;
+const STORAGE_NAMES_FILE = APP_DIR . 'storage' . DIRECTORY_SEPARATOR . 'names.json';
