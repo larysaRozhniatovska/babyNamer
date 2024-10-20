9de15cceed02875118ab37c1af41ef7683143a60
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-20 21:58:18 +0300

    add css style.css link

--- app/views/templates/template_main.php
+++ app/views/templates/template_main.php
@@ -6,6 +6,7 @@
                   content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title><?= $page?><?= TEXT_SITE_NAME?></title>
+            <link rel="stylesheet" href="<?=CSS_DIR . 'styles.css'?>">
     </head>
     <body>
         <?php include_once VIEWS_COMMON_DIR . 'header.php'?>
