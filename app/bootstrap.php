1df002f2fe9f56733db1e80522d36fad4947076f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-20 21:50:10 +0300

    add bootstrap

---
+++ app/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+const APP_DIR = 'app'.DIRECTORY_SEPARATOR;
+include_once APP_DIR . 'config.php';
+include_once FUNCTIONS_DIR . 'controllers.php';
+include_once FUNCTIONS_DIR . 'storages.php';
+include_once FUNCTIONS_DIR . 'responses.php';
