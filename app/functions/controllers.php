64ec6ceccfe951e330f52ebf3eec0b6b7a450d2f
Larisa Rozhniatovska <lara_rozn@urk.net>
2024-10-20 21:48:55 +0300

    add function controllers - home

---
+++ app/functions/controllers.php
@@ -0,0 +1,17 @@
+<?php
+function home()
+{
+//    $names = getNames();
+//    $girlsSize = count($names['girls']);
+//    $boysSize = count($names['boys']);
+//    if ($girlsSize > $boysSize) {
+//        $size = $girlsSize;
+//    } else {
+//        $size = $boysSize;
+//    }
+    render('home');
+//    render('home', [
+//        'names' => $names,
+//        'size' => $size,
+//    ]);
+}
